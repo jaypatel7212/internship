8ac00f581dabc1be891929e241e0610dd2a7ec27
jaypatel7212 <jaypatel.lamprostech@gmail.com>
2023-02-17 11:55:00 +0530

    day-9

---
+++ javascript_lampros/day-6/document.js
@@ -0,0 +1,10 @@
+// document.body.style.background = 'red'; // make the background red
+
+// setTimeout(() => document.body.style.background = '', 3000); // return back
+
+
+
+//------>>>>>>>>>  events   <<<<<-----------------//
+
+// like click , key press, key up , double click , right click , mouse hover , mouse out 
+// mouse down , mouse up , load ,  unload , resize , scroll
